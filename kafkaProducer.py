8149aca2de529195e4b3e1a34ba7b03ed7cefff8
Jinhan Choi <choi.jinhan@gmail.com>
2017-07-31 11:58:38 +0900

    indent

--- kafkaProducer.py
+++ kafkaProducer.py
@@ -5,37 +5,36 @@ import random
 import datetime
 
 from kafka import KafkaProducer
+
 producer = KafkaProducer(bootstrap_servers='master:6667')
 kafkaTopicName = 'test'
 
 if len(sys.argv) != 5:
-        print "Usage: [filename].py chSize, chCountMax, msgCount, msgRate(kbps)"
-        exit(-1)
+    print("Usage: [filename].py chSize, chCountMax, msgCount, msgRate(kbps)")
+    exit(-1)
+    msgRate = int(sys.argv[4])
+    byteSent = 0
+    startTime = datetime.datetime.now()
 
-msgRate = int(sys.argv[4])
-byteSent = 0
-startTime = datetime.datetime.now()
-
-for _ in range(int(sys.argv[3])):
+    for _ in range(int(sys.argv[3])):
         chDict = {}
         chDict['time'] = int(time.time())
         chTemp = {}
         for i in range(10):
-                chName = "CH_" + str(random.randint(1, int(sys.argv[1])))
-                chVal = random.randint(0, int(sys.argv[2]))
-                chTemp[chName] = chVal
-        chDict['data'] = chTemp
-        result = json.dumps(chDict)
-        print(result)
-        producer.send(kafkaTopicName, result)
-        byteSent += len(result)
-        expectedTime = (byteSent * 8.0) / (msgRate * 1024)
-        elapsedTime = datetime.datetime.now() - startTime
-        while elapsedTime.total_seconds() < expectedTime:
+            chName = "CH_" + str(random.randint(1, int(sys.argv[1])))
+            chVal = random.randint(0, int(sys.argv[2]))
+            chTemp[chName] = chVal
+            chDict['data'] = chTemp
+            result = json.dumps(chDict)
+            print(result)
+            producer.send(kafkaTopicName, result)
+
+            byteSent += len(result)
+            expectedTime = (byteSent * 8.0) / (msgRate * 1024)
+            elapsedTime = datetime.datetime.now() - startTime
+            while elapsedTime.total_seconds() < expectedTime:
                 time.sleep(0.001)
                 elapsedTime = datetime.datetime.now() - startTime
-
-endTime = datetime.datetime.now()
-totalTime = endTime - startTime
-print "Run " + str(totalTime.total_seconds()) + "sec"
-
+                endTime = datetime.datetime.now()
+                totalTime = endTime - startTime
+                print("Run " + str(totalTime.total_seconds()) + "sec")
